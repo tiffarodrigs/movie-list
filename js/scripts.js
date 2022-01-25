e9300bce2081eb5273237ccc46a741ea1ac9c08d
Eric Crudup <briefcasemanx@gmail.com>
2022-01-25 13:16:40 -0800

    adds Ticket() object
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
+function Ticket(movieName,time,age){
+  this.movieName = movieName;
+  this.time = time;
+  this.age = age;
+
+}
+Ticket.prototype.price=function(){
+  if (this.movie === newRealease) {
+    if (this.time === evening) {
+      this.price = 50;
+    } else if (this.time === afternoon) {
+      this.price = 40;
+    } else if (this.time === morning || this.age > 60) {
+      this.price = 30;
+    } else {
+      this.price = 40;
+    }
+  } else {
+    this.price = 25;
+  }
+  return this.price
+}
+
+
+
+
 $(document).ready(function() {
+  
+  $("#userInfo").submit( function() {
+    event.preventDefault();
+  const
+})
 
 
 
