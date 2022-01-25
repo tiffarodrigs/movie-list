af8de9d1edd76689304f166b28a44a2c742ad474
Eric Crudup <briefcasemanx@gmail.com>
2022-01-25 13:56:47 -0800

    functionality for displaying tickets
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,13 @@ function Ticket(movieName,time,age){
 
 }
 Ticket.prototype.price=function(){
-  if (this.movie === newRealease) {
-    if (this.time === evening) {
+  console.log(this.movieName);
+  if (this.movieName === "new-release") {
+    if (this.time === "evening") {
       this.price = 50;
-    } else if (this.time === afternoon) {
+    } else if (this.time === "afternoon") {
       this.price = 40;
-    } else if (this.time === morning || this.age > 60) {
+    } else if (this.time === "morning" && this.age === "over-60") {
       this.price = 30;
     } else {
       this.price = 40;
@@ -21,14 +22,32 @@ Ticket.prototype.price=function(){
   return this.price
 }
 
+function displayTicket( movieName,movieTime,movieAge,moviePrice)
+{
+$("#show-ticket").show();
+$(".movie-name").html(movieName);
+$(".time").html(movieTime)
+$(".age").html(movieAge)
+$(".price").html(moviePrice)
 
+}
 
 
 $(document).ready(function() {
   
   $("#userInfo").submit( function() {
     event.preventDefault();
-  const
+  
+  const inputName=$("#movie-type").val();
+  const inputTime=$("#time-of-day").val();
+  const inputAge=$("#age").val();
+
+  let ticket=new Ticket(inputName,inputTime,inputAge);
+  const moviePrice = ticket.price();
+  //console.log("price" +moviePrice);
+  displayTicket(inputName,inputTime,inputAge,moviePrice);
+
+
 })
 
 
